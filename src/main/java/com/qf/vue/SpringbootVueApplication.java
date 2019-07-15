8d03dfe718b7c8b7d9c694828a667432cb0abb9b
ZhangDian <1509251585@qq.com>
2019-07-15 16:45:03 +0800

    1.0版本

---
+++ src/main/java/com/qf/vue/SpringbootVueApplication.java
@@ -0,0 +1,14 @@
+package com.qf.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ZhangDian on 2019/7/15.
+ */
+@SpringBootApplication
+public class SpringbootVueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVueApplication.class,args);
+    }
+}
